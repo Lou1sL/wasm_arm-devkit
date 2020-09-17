2166429355cb09ce97e2374d112bc199a08883cd
RyuBAI <me@ryubai.com>
2020-09-18 00:14:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 
 
 const char moo[] = 
-    "^__^               \n"
-    "(oo)\\_______       \n"
-    "(__)\\       )\\/\\   \n"
-    "    ||----w |      \n"
-    "    ||     ||      \n"
-    "\n"
-    "WELCOME TO WASM_ARM!\n";
+    "^__^\n"
+    "(oo)\\_______\n"
+    "(__)\\       )\\/\\\n"
+    "    ||----w |\n"
+    "    ||     ||\n\n"
+    "WELCOME TO WASM_ARM!!\n";
 
 int main(void){
     warm::print(moo, strlen(moo));
