84da397ad0fb991ed79773c04ab3fc7d531d7aee
RyuBAI <me@ryubai.com>
2020-09-25 18:10:03 +0800

    Update io.h

--- warmlib/io.h
+++ warmlib/io.h
@@ -2,7 +2,7 @@
 #include <cstring>
 #include <cinttypes>
 
-volatile uint8_t * const scr_buffer = (uint8_t *)0x001FF00;
+volatile uint8_t* const scr_buffer = (uint8_t*)0x001FF00;
 uint8_t warm_scr_ptr = 0;
 void clear(){
     warm_scr_ptr = 0;
@@ -10,13 +10,15 @@ void clear(){
         scr_buffer[i] = 0x20;
     }
 }
-void print(const char* str){
-    size_t len = strlen(str);
-    for(std::size_t i=0; i<len; i++){
+void print(const char* str, size_t size){
+    for(std::size_t i=0; i<size; i++){
         if(str[i] == '\n' || str[i] == '\r'){
             warm_scr_ptr = (warm_scr_ptr + 0b00100000) & 0b11100000;
         }else{
             scr_buffer[warm_scr_ptr++] = str[i];
         }
     }
+}
+void print(const char* str){
+    print(str, strlen(str));
 }
