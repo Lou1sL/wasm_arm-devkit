ddd6c2d5685adc8edcc18a2d61835aecb3f37b1d
RyuBAI <me@ryubai.com>
2020-09-26 20:20:45 +0800

    dynamic cast

--- main.cpp
+++ main.cpp
@@ -1,39 +1,14 @@
 #include "main.h"
 
-
-
-class TEST_CLASS {
-public:
-    TEST_CLASS(){
-        print("CONSTRUCTOR!\n");
-        SET_UND_EXCEP_HANDLER([](){ print("EXCEP UND!\n"); });
-        SET_SWI_EXCEP_HANDLER([](){ print("EXCEP SWI!\n"); });
-    }
-
-    ~TEST_CLASS(){
-        print("DESTROYED!\n");
-        SET_UND_EXCEP_HANDLER([](){});
-        SET_SWI_EXCEP_HANDLER([](){});
-    }
-    int DumbFuncCall(uint32_t x, uint32_t y);
-    void PrintMoo();
-    void TestIRQ();
-private:
-    const char moo[256] = 
-        "^__^\n"
-        "(oo)\\_______\n"
-        "(__)\\       )\\/\\\n"
-        "    ||----w |\n"
-        "    ||     ||\n\n"
-        "WELCOME TO WASM_ARM!!\n";
-};
 int TEST_CLASS::DumbFuncCall(uint32_t x = 0x33443344, uint32_t y = 0x11221122){
     return 0x55665566;
 }
-void TEST_CLASS::PrintMoo(){
+void TEST_CLASS::Moo(){
     print(this->moo);
 }
-void TEST_CLASS::TestIRQ(){
+void TEST_CLASS::TestInterrupt(){
+    SET_UND_EXCEP_HANDLER([](){ print("EXCEP UND!\n"); });
+    SET_SWI_EXCEP_HANDLER([](){ print("EXCEP SWI!\n"); });
     print("IRQ ENABLED!\n");
     SET_IRQ_EXCEP_HANDLER([](){ print("IRQ BEFORE/DURING PRINT!\n"); });
     ARM_CBIT_LOCK lock = ARM_LOCK_CBIT(ARM_CBIT_SYS_INT_ENABLE);
@@ -44,15 +19,37 @@ void TEST_CLASS::TestIRQ(){
     print("I'm AFTER PRINT!\n");
     ARM_UNLOCK_CBIT(lock);
     print("IRQ DISABLED!\n");
+    SET_UND_EXCEP_HANDLER([](){});
+    SET_SWI_EXCEP_HANDLER([](){});
 }
+void TEST_CLASS::TestVirtual()  { print("I'm BASE!\n");    }
+void TEST_VIRTUAL::TestVirtual(){ print("I'm DERIVED!\n"); }
+
+TEST_CLASS::TEST_CLASS()     { print("BASE CONSTRUCTOR!\n");  }
+TEST_CLASS::~TEST_CLASS()    { print("BASE DESTROYED!\n");    }
+TEST_VIRTUAL::~TEST_VIRTUAL(){ print("DERIVED DESTROYED!\n"); }
 
 int main(void){
-    TEST_CLASS* test = new TEST_CLASS();
-    __attribute__((unused))
-    int a = test->DumbFuncCall();
-    test->PrintMoo();
-    test->TestIRQ();
-    delete test;
+
+    TEST_CLASS* base = new TEST_CLASS();
+    TEST_VIRTUAL* derived = new TEST_VIRTUAL();
+    //__attribute__((unused))
+    //int a = test_base->DumbFuncCall();
+    //test_base->Moo();
+    //test_base->TestInterrupt();
+    
+
+
+    TEST_CLASS* test = base;
+    test->TestVirtual();
+    test = derived;
+    test->TestVirtual();
+    TEST_VIRTUAL* test2 = dynamic_cast<TEST_VIRTUAL*>(test);
+    if(test2 == nullptr) print("DOWNCAST FAILED!\n");
+    else test2->TestVirtual();
+
+    delete base;
+    delete derived;
     return 0;
 }
 
--- main.h
+++ main.h
@@ -2,3 +2,26 @@
 
 
 
+class TEST_CLASS {
+public:
+    TEST_CLASS();
+    virtual ~TEST_CLASS();
+    int DumbFuncCall(uint32_t x, uint32_t y);
+    void Moo();
+    void TestInterrupt();
+    virtual void TestVirtual();
+private:
+    const char moo[256] = 
+        "^__^\n"
+        "(oo)\\_______\n"
+        "(__)\\       )\\/\\\n"
+        "    ||----w |\n"
+        "    ||     ||\n\n"
+        "WELCOME TO WASM_ARM!!\n";
+};
+
+class TEST_VIRTUAL : public TEST_CLASS {
+public:
+    ~TEST_VIRTUAL();
+    void TestVirtual();
+};
