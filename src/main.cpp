fd75686b6d6e36bb7f262204de1b5ed703931a6d
RyuBAI <me@ryubai.com>
2021-02-19 19:08:15 +0800

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,19 @@
 #include "main.h"
 
 
+class A{
+public:
+    virtual int Foo() { return 0; }
+};
 
+class B : public A {
+public:
+    int Foo() { return 2; }
+};
 
-
-
-
-
-
-int foo(int numbers...) {
-    long int z = 0xCCDDEEFF;
-    long int y = 0x8899AABB;
-    long int w = 0x44556677;
-    long int v = 0x00112233;
-    for(;;);
-    return 0;
-}
 int main() {
-    //ArmTest();
-    //foo(std::make_integer_sequence<int, 0x20>());
+    A* a = new B();
+    B* b = dynamic_cast<B*>(a);
+
     return 0;
 } 
