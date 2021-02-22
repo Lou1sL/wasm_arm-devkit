973a7b49df9600ac37f91d0553437082317db36b
RyuBAI <me@ryubai.com>
2021-02-23 07:00:38 +0800

    Update run.sh

--- callgraph/run.sh
+++ callgraph/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-clang-11 -O0 -S -std=c++17 -emit-llvm graph_main.cpp -o - | opt-11 -std-link-opts -analyze -dot-callgraph -o /dev/null
 # sudo apt-get install graphviz
 # sudo apt-get install gawk
-cat '<stdin>.callgraph.dot' | c++filt | sed 's,>,\\>,g; s,-\\>,->,g; s,<,\\<,g' | gawk '/external node/{id=$1} $1 != id' | dot -Tpng -o callgraph.png
+
+rm '<stdin>.callgraph.dot' callgraph.png
+
+clang-11 -S -std=c++17 -emit-llvm graph_main.cpp -o - | opt-11 -std-link-opts -analyze -dot-callgraph -o /dev/null
+# dot '<stdin>.callgraph.dot' -Tpng -o callgraph.png
+# cat '<stdin>.callgraph.dot' | c++filt | sed 's,>,\\>,g; s,-\\>,->,g; s,<,\\<,g' | gawk '/external node/{id=$1} $1 != id' | dot -Tpng -o callgraph.png
